7990a4d57a54ca3ba71ae3236847b73b3c54da7a
th1460 <thop100@hotmail.com>
2023-04-09 16:50:44 -0300

    first commit

---
+++ atom_motion.py
@@ -0,0 +1,65 @@
+import machine
+import struct
+import network
+import socket
+
+# setup servo and motor
+sda_pin = machine.Pin(25)
+scl_pin = machine.Pin(21)
+
+i2c = machine.I2C(0, sda=sda_pin, scl=scl_pin, freq=400000)
+devices = i2c.scan()
+
+device = [i for i in devices if hex(i) == '0x38'][0]
+
+def set_speed(speed):
+    buf = bytearray(1)
+    struct.pack_into('b', buf, 0, speed)
+    i2c.writeto_mem(device, 0, buf)
+
+def get_speed():
+    return struct.unpack('b', i2c.readfrom_mem(device, 0, 1))[0]
+
+def set_angle(angle):
+    buf = bytearray(1)
+    struct.pack_into('b', buf, 0, angle)
+    i2c.writeto_mem(device, 2, buf)
+
+def get_angle():
+    return struct.unpack('b', i2c.readfrom_mem(device, 2, 1))[0]
+
+
+def set_direction(x):
+    if x > 200:
+        set_angle(65)
+    elif x < 50:
+        set_angle(115)
+    else:
+        set_angle(90)
+        
+
+def set_run(x):
+    if x > 200:
+        set_speed(127)
+    elif x < 50:
+        set_speed(0)
+    else:
+        set_speed(86)
+
+# create access-point
+ap = network.WLAN(network.AP_IF)
+ap.config(essid='ATOM-MOTION')
+ap.config(max_clients=10)
+ap.active(True)
+
+# create server socket
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.bind(('', 12000))
+
+while True:
+    direction, address_client = server.recvfrom(2048)
+    out = struct.unpack('BBB', direction)
+    set_direction(out[0])
+    set_run(out[1])
+    print(out)
+
---
+++ joystick.py
@@ -0,0 +1,40 @@
+import machine
+import struct
+import network
+import socket
+import neopixel
+import time
+
+
+# setup led
+np = neopixel.NeoPixel(machine.Pin(27), 1)
+
+# setup joystick
+sda_pin = machine.Pin(26)
+scl_pin = machine.Pin(32)
+
+i2c = machine.I2C(0, sda=sda_pin, scl=scl_pin, freq=400000)
+devices = i2c.scan()
+device = [i for i in devices if hex(i) == '0x52'][0]
+
+# create access-point
+def ap_connect():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect('ATOM-MOTION')
+        while not wlan.isconnected():
+            pass
+    print('network config', wlan.ifconfig())
+
+ap_connect()
+
+# create client socket
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+np[0] = (0,255,0)
+np.write()
+
+while True:
+    client.sendto(i2c.readfrom(device, 3), ('192.168.4.1', 12000))
+    time.sleep(1)
