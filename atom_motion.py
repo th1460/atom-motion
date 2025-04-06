d72f94393ec9f01d44469a905cfa5fc4fa70cad9
Thiago-Pires <thiago.pires@ibm.com>
2025-04-06 14:57:22 -0300

    feat: remove unitv

--- atom_motion.py
+++ atom_motion.py
@@ -5,10 +5,6 @@ import socket
 import neopixel
 import utime
 
-# setup uart
-uart = machine.UART(1, tx=19, rx=22)
-uart.init(115200, bits=8, parity=None, stop=1)
-
 # setup led
 np = neopixel.NeoPixel(machine.Pin(27), 1)
 
@@ -19,17 +15,20 @@ scl_pin = machine.Pin(21)
 i2c = machine.I2C(0, sda=sda_pin, scl=scl_pin, freq=400000)
 devices = i2c.scan()
 
-device = [i for i in devices if hex(i) == '0x38'][0]
+device = [i for i in devices if hex(i) == "0x38"][0]
+
 
 def set_speed(speed):
     buf = bytearray(1)
-    struct.pack_into('b', buf, 0, speed)
+    struct.pack_into("b", buf, 0, speed)
     i2c.writeto_mem(device, 0, buf)
+
 
 def set_angle(angle):
     buf = bytearray(1)
-    struct.pack_into('b', buf, 0, angle)
+    struct.pack_into("b", buf, 0, angle)
     i2c.writeto_mem(device, 2, buf)
+
 
 def set_direction(x):
     if x > 200:
@@ -38,7 +37,8 @@ def set_direction(x):
         set_angle(115)
     else:
         set_angle(90)
-        
+
+
 def set_run(x):
     if x > 200:
         set_speed(127)
@@ -47,37 +47,30 @@ def set_run(x):
     else:
         set_speed(86)
 
+
 # create access-point
 ap = network.WLAN(network.AP_IF)
-ap.config(essid='ATOM-MOTION')
+ap.config(essid="ATOM-MOTION")
 ap.config(max_clients=10)
 ap.active(True)
 
 # create server socket
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.bind(('', 12000))
+server.bind(("", 12000))
 
 while True:
-    
+
     try:
-        status = int(uart.read(1).decode())
-        if status == 1:
-            np[0] = (0,0,255)
-            np.write()
-        elif status == 0:
-            np[0] = (0, 255, 0)
-            np.write()
-            direction, address_client = server.recvfrom(2048)
-            out = struct.unpack('BBB', direction)
-            set_direction(out[0])
-            set_run(out[1])
-            print(out)
-        else:
-            np[0] = (255, 0, 0)
-            np.write()
-        print(status)
+        np[0] = (0, 255, 0)
+        np.write()
+        direction, address_client = server.recvfrom(2048)
+        out = struct.unpack("BBB", direction)
+        set_direction(out[0])
+        set_run(out[1])
+        print(out)
+
     except:
         pass
-    
+
     utime.sleep_ms(500)
